bb7d9a0b43812d8e5e06b00729fc5947d2041c4b
Chris Buttacavoli <chris.buttacavoli@a-dec.com>
2018-06-01 15:41:00 -0700

    Created Main() function

--- runants.py
+++ runants.py
@@ -9,36 +9,31 @@ import math
 from drawGraph import plotTour
 
 
+def Main(inputFile):
+	# Setup up our variables
+	writeFile = "mytour" # + ".tour"
+	numIterations = 1
+	cities, cityIds = ReadCityData(inputFile)
+	graph = Graph(cities, cityIds) # Each edge of the graph has its own pheromones
+	bestTour = Tour(cities[0])
+	bestTour.tourLength = float('inf')
+
+	# This is our algorithm
+	for n in range(0, numIterations):
+		# New ants are placed at each city every iteration
+		ants = [Ant(cityIds, startCity) for startCity in cities]
+		for ant in ants:
+			while ant.hasNotCompletedTour:
+				ant.moveToNextCity(graph)
+			bestTour = ReturnBestTour(ant.tour, bestTour)
+		graph.updatePheromones(ants, cities)
+	OutputData(writeFile, bestTour)
+
+
 def ReturnBestTour(tour1, tour2):
 	if tour1.tourLength > tour2.tourLength:
 		return tour2
 	return tour1
-
-
-###########################
-# Main procedure below...
-###########################
-###########################
-
-# Setup up our variables
-readFile = sys.argv[1]
-writeFile = "mytour" # + ".tour"
-numIterations = 1
-cities, cityIds = ReadCityData(readFile)
-graph = Graph(cities, cityIds) # Each edge of the graph has its own pheromones
-bestTour = Tour(cities[0])
-bestTour.tourLength = float('inf')
-
-# This is our algorithm
-for n in range(0, numIterations):
-	# New ants are placed at each city every iteration
-	ants = [Ant(cityIds, startCity) for startCity in cities]
-	for ant in ants:
-		while ant.hasNotCompletedTour:
-			ant.moveToNextCity(graph)
-		bestTour = ReturnBestTour(ant.tour, bestTour)
-	graph.updatePheromones(ants, cities)
-OutputData(writeFile, bestTour)
 
 
 ## i tried a matplot demo on flip, but
@@ -49,3 +44,7 @@ OutputData(writeFile, bestTour)
 ## -- submit our code. This will just be for
 ## -- us to visually confirm our results
 # plotTour(cities, bestTour)
+
+# Allows us to put the Main() function at the top of the file
+if __name__ == '__main__':
+	Main(sys.argv[1])
