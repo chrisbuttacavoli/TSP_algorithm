a92995d64b493ce1c2413cd14402c97d59d3e132
mikesio <32244418+mikesio@users.noreply.github.com>
2018-06-02 01:42:36 +0800

    drawGraph.py added
    also some logical changes on Ant class.
    runants.py is modified to reflect those changes.

--- runants.py
+++ runants.py
@@ -3,13 +3,14 @@ from classes import City, Graph, Ant
 from fileOperations import ReadCityData, OutputData
 import sys
 import math
-
+from drawGraph import plotTour
 
 ###########################
 # Main procedure below...
 ###########################
 ###########################
 
+bestTourLength = float('inf')
 numIterations = 20
 cities = ReadCityData(sys.argv[1])
 graph = Graph(cities) # Each edge of the graph has its own pheromones
@@ -17,11 +18,19 @@ ants = [Ant(len(cities), city) for city in cities] # Start an ant at each city
 
 for n in range(1, numIterations):
 	for ant in ants:
-		while ant.hasNotCompletedTour:
-			ant.move(graph)
-		bestTour = SelectBestTour(ant.tour, bestTour)
+		ant.move(graph)
+		#update best tour
+		if(ant.tour.tourLength < bestTourLength):
+            bestTourLength = ant.tour.tourLength
+            bestPath = ant.tour.path
 	
 	graph.updatePheromones(ants)
 
-OutputData(bestTour)
+
+## i tried a matplot demo on flip, but
+## it seems output figure cannot be displayed
+## on filp due to the lack of GUI 
+## we may replace this by a simple print
+plotTour(cities, bestPath, bestTourLength)
+
 ###########################
